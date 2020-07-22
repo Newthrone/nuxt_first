aa3fb98f6a32f71d0c16c5a3934b8bd8a995cd50
Vyacheslav <vyacheslav.belozerov@rutorika.ru>
2020-07-22 20:02:15 +0300

    start && update

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nuxt-link class="navbar-brand" to="/">Nuxt SSR</nuxt-link>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" exact to="/">Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" no-prefetch class="nav-link" to="/about">About</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" no-prefetch class="nav-link" to="/users">Users</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link active-class="active" no-prefetch class="nav-link" to="/login">Login</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Apout page</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, maxime odit aspernatur voluptatibus maiores rerum, distinctio culpa voluptatem ratione quis modi nulla suscipit quaerat tempore fugiat eligendi totam accusamus doloremque?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus nam autem numquam laborum, assumenda architecto, iusto labore necessitatibus facere odio soluta fugit. Quia dolorem veniam maxime iusto. Nemo, dolor dolores!Veritatis velit vero asperiores aliquid nulla! Est necessitatibus nam magni fuga consequuntur, quidem dolorem ipsum voluptates iusto expedita quas illum voluptatibus culpa quo explicabo. Ad ratione odio sit sint corporis.</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
